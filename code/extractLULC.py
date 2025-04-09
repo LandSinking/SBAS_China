5d2b5e28fb5ff822d4c2097811879288b8079f0b
LandSinking <148524195+LandSinking@users.noreply.github.com>
2025-04-09 16:00:27 +0800

    Update extractLULC.py

--- code/extractLULC.py
+++ code/extractLULC.py
@@ -7,6 +7,8 @@ from pathlib import Path
 import h5py
 import load2hdf5
 import osgeo_utils.gdal_calc as gc
+
+
 def getProjBoundFromHDF5(filename):    
     # f = h5py.File("velocity.h5", "r") 
     f = h5py.File(filename, "r") 
@@ -53,13 +55,12 @@ def isBoundOverlap(b1,b2):
     else:
         return True
 def clipByMask(files, h5Filename,outputfile):
-    prefix=h5Filename.parent.stem
-    finalfile=f'{prefix}.tif'
+    prefix=h5Filename.parent.stem    
     h5ProjSRS,h5Trans,LENGTH,WIDTH,h5ProjBound=getProjBoundFromHDF5(h5Filename)
     h5GeoBound=ProjBound2GeoBound(h5ProjSRS,h5Trans,LENGTH,WIDTH)
     overlappedFiles=[]    
     for i in range(len(files)):
-        infile = str(files[i])   
+        infile = str(files[i])
         outfile = f'{prefix}_{i}.tif'
         filehandle = gdal.Open(infile)
         imgTrans = filehandle.GetGeoTransform() # t1
@@ -90,30 +91,3 @@ def clipByMask(files, h5Filename,outputfile):
         for currFile in overlappedFiles:
             Path(currFile).unlink()
     return 
-'''
-parser = argparse.ArgumentParser(
-description='Acquire some parameters for fusion restore')
-parser.add_argument('-c', '--config', default="",help='path to config file (*.h5)')
-parser.add_argument('-l', '--lulcpath', required=True, help='path to LULC files (*.tif)') # 
-opt = parser.parse_args()
-
-
-lulcFiles = list(Path(opt.lulcpath).glob('*0101.tif'))
-
-h5Files=list(Path(opt.config).glob('CN_*/geometryGeo.h5'))
-
-for currFile in h5Files:
-    print('Processing',str(currFile),'...')
-    clipByMask(lulcFiles, currFile)   
-
-currFile=Path(opt.config)
-print('Processing',str(currFile),'...')
-lulcTiFF='H:\SAR\workplace_CN_\Mintpy\CN_XG_lulc.tif'
-# clipByMask(lulcFiles, currFile,lulcTiFF)   
-watermaskTiFF = 'H:\SAR\workplace_CN_\Mintpy\CN_XG.tif'
-
-watermaskH5 = 'H:\SAR\workplace_CN_\waterMask.h5'
-# gdal_calc.py -A input.tif --outfile=result.tif --calc="A*(A>0)" --NoDataValue=0
-gc.main(['--quiet','-a',lulcTiFF, '--outfile',watermaskTiFF,'--calc', "a>1"])
-load2hdf5.main([watermaskTiFF, '--dtype', 'byte', '--dname', 'mask', '-o', watermaskH5,'--force'])
-'''
